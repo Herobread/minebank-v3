c136c1735c29d7815041d36d652e4e96db78b2a3
Alex Nazarenko <alexeybread81@gmail.com>
2023-03-14 15:19:18 +0000

    add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,18 @@ import MainLayout from '../components/layout/MainLayout/MainLayout'
 export default function Home() {
 	return (
 		<MainLayout>
+			<Header>Buttons</Header>
 			<Button>aaa</Button>
 			<Button disabled>aaa</Button>
 			<Button appearence="accent">aaa</Button>
+			<Button disabled appearence="accent">
+				aaa
+			</Button>
+
+			<Header>Input</Header>
+			<Input label="Username" error="Something is wrong" value={'abcde'} />
+			<Input label="Password" type="password" />
+			<Input label="Password 2" type="password" />
 		</MainLayout>
 	)
 }
